0467fd321ccfae030c356079491e11fdac1cc071
OLI <olicass100@gmail.com>
2024-10-15 12:12:02 +0100

    testing the api access

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&interval=1min&apikey=demo37QAPHW9NQALLU9I'
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=CFG&interval=30min&apikey=demo37QAPHW9NQALLU9I&outputsize=full'
 r = requests.get(url)
 data = r.json()
 
