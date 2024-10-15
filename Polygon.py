04d5b35856608c5ff96e2d4fc0834bc027320df5
OLI <olicass100@gmail.com>
2024-10-15 18:43:18 +0100

    new api trying

---
+++ Polygon.py
@@ -0,0 +1,16 @@
+import polygon
+
+client = polygon.RESTClient("DUEYmzwA2R9d8l5I18mNdycBZuHHYmXn")
+
+aggs = []
+for a in client.list_aggs(
+    "AAPL",
+    1,
+    "minute",
+    "2022-01-01",
+    "2023-02-03",
+    limit=50000,
+):
+    aggs.append(a)
+
+print(aggs)
