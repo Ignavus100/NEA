2a3153274886128cfa23f5c3e7edb6c32aa99de1
OLI <olicass100@gmail.com>
2024-10-19 18:03:58 +0100

    bars are working

--- Polygon.py
+++ Polygon.py
@@ -3,38 +3,50 @@ from pprint import pp
 import matplotlib
 from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
-plt.style.use("ggplot")
+plt.style.use("dark_background")
 import numpy as np
 from datetime import datetime
 
 client = polygon.RESTClient("DUEYmzwA2R9d8l5I18mNdycBZuHHYmXn")
 
 aggs = []
+start = "2024-10-17"
+end = "2024-10-18"
 for a in client.list_aggs(
     "AAPL",
     1,
-    "minute",
-    "1729172815270",
-    "1729173013946",
+    "hour",
+    start,
+    end,
     limit=5000,
 ):
     aggs.append(a)
 
+#plt.xlim(0, 10)
+p = []
 def animate(i):
-    #plt.cla()
-
+    #plt.cla())
     data = aggs[i]
-    x = data.timestamp
-    y = data.open
+    p.append(data.timestamp)
+    q1 = min(data.open, data.close)
+    q3 = max(data.open, data.close)
+    x = datetime.fromtimestamp(int(data.timestamp)/1000)
+    plt.boxplot([data.low, q1, data.close, q3, data.high], positions=[data.timestamp], widths=3600000)
+    
+#    x = datetime.fromtimestamp(int(data.timestamp)/1000)
+#    y = data.open
 
-    plt.plot(x, y, marker = ".")
-    y = data.close
-    plt.plot(x, y, marker = ".")
+#    plt.plot(x, y, marker = ".")
+#    y = data.close
+#    plt.plot(x, y, marker = ".")
+#    plt.fill_between(x, data.open, data.close)
 
+plt.xlabel("Date and Time")
+plt.ylabel("Price")
+plt.title(f"APPL on {start}")
 ani = FuncAnimation(plt.gcf(), animate, interval = 1000, frames = 500, repeat = False)
-#fig, ax = plt.subplots()
+
 #for i in range(len(aggs)):
  #   ax.plot(aggs[i].timestamp, aggs[i].close, markersize=100)
 plt.tight_layout()
 plt.show()
-pp(aggs[1].open)
---
+++ test.py
@@ -0,0 +1,2 @@
+date = "2023-01-09"
+print(int(date) + 1)
