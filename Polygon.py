fa914a1d599498dcf8fde4fac9467cb496aa2662
OLI <olicass100@gmail.com>
2024-11-05 09:58:18 +0000

    changing colour stules and plotting

--- Polygon.py
+++ Polygon.py
@@ -3,6 +3,7 @@ from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 plt.style.use("dark_background")
 import functools
+import numpy as np
 
 client = polygon.RESTClient("DUEYmzwA2R9d8l5I18mNdycBZuHHYmXn")
 
@@ -55,17 +56,17 @@ def GraphData():
         p.append(currentPoint.timestamp)
 
         #OC-checking the timeframe gaps
-        if (aggs[data.currentBase].timestamp + 1000000) < currentPoint.timestamp:
+        if (aggs[data.currentBase].timestamp + 3600000) < currentPoint.timestamp:
             data.NewBase(i)
             data.NewBar(aggs[i].low, aggs[i].high)
 
         #OC-defining the upper and lower quartiles
-        lq = min(aggs[data.currentBase].open, aggs[i].close)
-        if lq == aggs[data.currentBase].open:
+        lq = min(aggs[data.currentBase-1].close, aggs[i].close)
+        if lq == aggs[data.currentBase-1].close:
             bullish = False
         else:
             bullish = True
-        uq = max(aggs[data.currentBase].open, aggs[i].close)
+        uq = max(aggs[data.currentBase -1 ].close, aggs[i].close)
 
         #OC-making new bounds
         if data.lowest > aggs[i].low:
@@ -75,30 +76,30 @@ def GraphData():
 
         #OC-adding the bar to points in the position of how many bars there are
         try:
-            points[data.bars] = [data.lowest, lq, aggs[data.currentBase].open, uq, data.highest, bullish]
+            points[data.bars] = [data.lowest, lq, aggs[data.currentBase - 1].close, uq, data.highest, bullish]
         except:
-            points.append([data.lowest, lq, aggs[data.currentBase].open, uq, data.highest, bullish])
+            points.append([data.lowest, lq, aggs[data.currentBase - 1].close, uq, data.highest, bullish])
 
         #OC-making the bars
         for j in range(len(points)):
             if points[j][5]:
-                plt.boxplot(points[j][:5], positions=[j + 1], widths=0.8, patch_artist=True, showfliers=False, showcaps=False,
-                            boxprops=dict(facecolor="red", color="red"),
-                            whiskerprops=dict(color="red"),
-                            capprops=dict(color="red"),
-                            medianprops=dict(color="red"))
+                plt.boxplot(points[j][:5], positions=[j + 1], widths=0.8, patch_artist=True, showfliers=True, showcaps=False, whis=(0, 100),
+                            boxprops=dict(facecolor="#f23645", color="#f23645"),
+                            whiskerprops=dict(color="#f23645"),
+                            capprops=dict(color="#f23645"),
+                            medianprops=dict(color="#f23645"))
 
             else:
-                plt.boxplot(points[j][:5], positions=[j + 1], widths=0.8, patch_artist=True, showfliers=False, showcaps=False,
-                            boxprops=dict(facecolor="green", color="green"), 
-                            whiskerprops=dict(color="green"), 
-                            capprops=dict(color="green"), 
-                            medianprops=dict(color="green"))
+                plt.boxplot(points[j][:5], positions=[j + 1], widths=0.8, patch_artist=True, showfliers=True, showcaps=False, whis=(0, 100),
+                            boxprops=dict(facecolor="#089981", color="#089981"), 
+                            whiskerprops=dict(color="#089981"), 
+                            capprops=dict(color="#089981"), 
+                            medianprops=dict(color="#089981"))
 
     #OC-label axis and graph
     plt.xlabel("Date and Time")
     plt.ylabel("Price")
-    plt.title(f"{ticket} on {start}") 
+    plt.title(f"{ticket} on {start}")
     p=[]
     points = []
     data = UpdateData(0, aggs[0].low, aggs[0].high, 0)
