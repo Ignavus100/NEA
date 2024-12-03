2e0a74687d059c03b81d9f67c8c4d263f55f2301
OLI <olicass100@gmail.com>
2024-12-03 12:01:05 +0000

    sorting files and adding pictures

--- AlphaVantage.py
+++
@@ -1,9 +0,0 @@
-import requests
-from pprint import pp
-
-# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=CFG&interval=30min&apikey=demo37QAPHW9NQALLU9I&outputsize=full'
-r = requests.get(url)
-data = r.json()
-
-pp(data)
